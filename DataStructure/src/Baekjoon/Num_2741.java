ff00730cd652f08e63d2d804a09d41f80824fe39
sallyshin <46617568+sallyshin@users.noreply.github.com>
2020-03-28 03:06:19 +0900

    N 찍기

---
+++ DataStructure/src/Baekjoon/Num_2741.java
@@ -0,0 +1,26 @@
+package Baekjoon;
+/*
+ * 문제
+자연수 N이 주어졌을 때, 1부터 N까지 한 줄에 하나씩 출력하는 프로그램을 작성하시오.
+
+입력
+첫째 줄에 100,000보다 작거나 같은 자연수 N이 주어진다.
+ */
+import java.io.*;
+
+public class Num_2741 {
+
+	public static void main(String[] args) throws Exception {
+		
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		int n = Integer.parseInt(br.readLine());
+		
+		for(int i = 1; i <= n; i++) {
+			System.out.println(i);
+		}
+		
+
+	}
+
+}
