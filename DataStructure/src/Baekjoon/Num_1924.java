a4a58e75b052277d21bdf44b5f4d28098e7cdc35
sallyshin <46617568+sallyshin@users.noreply.github.com>
2020-03-29 00:02:42 +0900

    2007년

--- DataStructure/src/Baekjoon/Num_1924.java
+++ DataStructure/src/Baekjoon/Num_1924.java
@@ -1,13 +1,5 @@
 package Baekjoon;
-/*
- * 문제
-오늘은 2007년 1월 1일 월요일이다. 그렇다면 2007년 x월 y일은 무슨 요일일까? 이를 알아내는 프로그램을 작성하시오.
-입력
-첫째 줄에 빈 칸을 사이에 두고 x(1≤x≤12)와 y(1≤y≤31)이 주어진다. 
-참고로 2007년에는 1, 3, 5, 7, 8, 10, 12월은 31일까지, 4, 6, 9, 11월은 30일까지, 2월은 28일까지 있다.
-출력
-첫째 줄에 x월 y일이 무슨 요일인지에 따라 SUN, MON, TUE, WED, THU, FRI, SAT중 하나를 출력한다.
- */
+
 import java.io.*;
 import java.util.*;
 public class Num_1924 {
@@ -15,12 +7,47 @@ public class Num_1924 {
 	public static void main(String[] args) throws Exception {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st = new StringTokenizer(br.readLine());
-		
+
 		int x = Integer.parseInt(st.nextToken());
 		int y = Integer.parseInt(st.nextToken());
-		
-		
-		
+		int days = 0;
+
+		int[] mth = {0, 0, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30};
+
+		for(int i = 1; i <= x; i++) {
+
+			days += mth[i];
+
+		}
+
+		days += y;
+
+		switch(days % 7) {
+		case 0:
+			System.out.println("SUN");
+			break;
+		case 1 :
+			System.out.println("MON");
+			break;
+		case 2:
+			System.out.println("TUE");
+			break;
+		case 3:
+			System.out.println("WED");
+			break;
+		case 4:
+			System.out.println("THU");
+			break;
+		case 5:
+			System.out.println("FRI");
+			break;
+		case 6:
+			System.out.println("SAT");
+			break;
+		}
+
+		br.close();
+
 
 	}
 
