cee214e052f0f83c7372f2cc393f6ddcfb5d26b4
sallyshin <46617568+sallyshin@users.noreply.github.com>
2020-03-31 22:21:05 +0900

    별찍기 - 5

---
+++ DataStructure/src/Baekjoon/Num_2442.java
@@ -0,0 +1,21 @@
+package Baekjoon;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
+
+public class Num_2442 {
+
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int n = Integer.parseInt(br.readLine());
+		
+		
+		for(int i = 0; i < n; i++) {
+			
+			
+		}
+		
+	}
+
+}
