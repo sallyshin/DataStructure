03a117945b98b68105c04bef7be91d4fe60997f3
sallyshin <46617568+sallyshin@users.noreply.github.com>
2020-03-31 23:24:12 +0900

    스택

--- DataStructure/src/BOJ_Lec_02/Num_10828.java
+++ DataStructure/src/BOJ_Lec_02/Num_10828.java
@@ -1,39 +1,63 @@
 package BOJ_Lec_02;
 
-/*
- * 문제
-정수를 저장하는 스택을 구현한 다음, 입력으로 주어지는 명령을 처리하는 프로그램을 작성하시오.
-명령은 총 다섯 가지이다.
-push X: 정수 X를 스택에 넣는 연산이다.
-pop: 스택에서 가장 위에 있는 정수를 빼고, 그 수를 출력한다. 만약 스택에 들어있는 정수가 없는 경우에는 -1을 출력한다.
-size: 스택에 들어있는 정수의 개수를 출력한다.
-empty: 스택이 비어있으면 1, 아니면 0을 출력한다.
-top: 스택의 가장 위에 있는 정수를 출력한다. 만약 스택에 들어있는 정수가 없는 경우에는 -1을 출력한다.
-입력
-첫째 줄에 주어지는 명령의 수 N (1 ≤ N ≤ 10,000)이 주어진다. 둘째 줄부터 N개의 줄에는 명령이 하나씩 주어진다.
- 주어지는 정수는 1보다 크거나 같고, 100,000보다 작거나 같다. 문제에 나와있지 않은 명령이 주어지는 경우는 없다.
-출력
-출력해야하는 명령이 주어질 때마다, 한 줄에 하나씩 출력한다.
- */
 import java.util.Stack;
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.io.IOException;
+import java.util.StringTokenizer;
 
 public class Num_10828 {
 
 	public static void main(String[] args) throws Exception {
-		
+
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		
+
 		int N = Integer.parseInt(br.readLine());
-		
-		Stack<Integer> stack = new Stack<>();
+
+		Stack<Integer> stack = new Stack<Integer>();
+
 		
 		for(int i = 0; i < N; i++) {
 			
+			StringTokenizer st = new StringTokenizer(br.readLine()," ");
+
+			String order = st.nextToken();
+
+			if(order.equals("push")) { // 넣는연산
+				int num = Integer.parseInt(st.nextToken());
+				stack.add(num);
+			}
+			else if(order.equals("pop")) { // 빼고, 출력, 빈 스택이면 -1 출력
+				if(stack.isEmpty()) {
+					System.out.println(-1);
+				}
+				else {
+					System.out.println(stack.pop());
+				}
+			}
+			else if(order.equals("size")) { // 스택에 있는 정수갯수 출력
+				System.out.println(stack.size());
+			}
+			else if(order.equals("empty")) {
+				if(stack.isEmpty()) {
+					System.out.println(1);
+				}
+				else {
+					System.out.println(0);
+				}
+			}
+			else if(order.equals("top")) {
+				if(stack.isEmpty()) {
+					System.out.println(-1);
+				}
+				else {
+					System.out.println(stack.peek());
+				}
+			}
 		}
 
+		br.close();
+		
 	}
 
 }
