e332806a119e2df38d58565895205ae840a9ff81
sallyshin <46617568+sallyshin@users.noreply.github.com>
2020-04-09 04:44:11 +0900

    미로탐색

---
+++ DataStructure/src/BOJ_Lec_02/Num_2178.java
@@ -0,0 +1,21 @@
+package BOJ_Lec_02;
+import java.io.*;
+import java.util.StringTokenizer;
+class Num_2178 {
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+
+		int n = Integer.parseInt(st.nextToken());
+		int m = Integer.parseInt(st.nextToken());
+		
+		for(int i = 0; i < n; i++) {
+			
+			for(int j = 0; j < m; j++) {
+				
+			}
+		}
+	}
+
+}
