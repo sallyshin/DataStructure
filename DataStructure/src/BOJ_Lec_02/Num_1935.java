27b08b7a03b7c7ddbc8528e39a491045eda1cadc
sallyshin <46617568+sallyshin@users.noreply.github.com>
2020-04-08 16:00:06 +0900

    후위표기식2

---
+++ DataStructure/src/BOJ_Lec_02/Num_1935.java
@@ -0,0 +1,48 @@
+package BOJ_Lec_02;
+import java.util.Stack;
+import java.io.IOException;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+public class Num_1935 {
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		Stack stack = new Stack();		
+		int n = Integer.parseInt(br.readLine());
+		double[] num = new double[n];
+		char[] exp = br.readLine().toCharArray();
+		
+		for(int i = 0; i < n; i++) {
+			num[i] = Integer.parseInt(br.readLine()); // 1 2 3 4 5 순서대로
+		}
+		
+		for(int i = 0; i < exp.length; i++) {
+			if(!Character.isUpperCase(exp[i])) {
+				double num2 = (double)stack.pop();
+				double num1 = (double)stack.pop();
+				
+				switch(exp[i]) {
+				case '+':
+					stack.push(num1 + num2);
+					break;
+				case '-':
+					stack.push(num1 - num2);
+					break;
+				case '*':
+					stack.push(num1 * num2);
+					break;
+				case '/':
+					stack.push(num1 / num2);
+					break;
+				}
+			}
+			else {
+				stack.push(num[exp[i] - 'A']); // 숫자로 넣기
+			}
+					
+		}
+		System.out.printf("%.2f", stack.pop());
+		
+	}
+
+}
