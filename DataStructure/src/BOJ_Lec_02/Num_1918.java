4afaa56610aa2a1f608f1d8632d5234c3f0c04cc
sallyshin <46617568+sallyshin@users.noreply.github.com>
2020-04-09 04:44:30 +0900

    후위 표기식

--- DataStructure/src/BOJ_Lec_02/Num_1918.java
+++ DataStructure/src/BOJ_Lec_02/Num_1918.java
@@ -1,9 +1,14 @@
 package BOJ_Lec_02;
-
+import java.util.Stack;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
 public class Num_1918 {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		char[] str = br.readLine().toCharArray();
+				
 
 	}
 
