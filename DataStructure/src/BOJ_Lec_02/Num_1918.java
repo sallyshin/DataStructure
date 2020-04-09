ea001d016bf664f98b4c723a7925e1dcafc7f6f0
sallyshin <46617568+sallyshin@users.noreply.github.com>
2020-04-10 03:22:26 +0900

    후위 표기식

--- DataStructure/src/BOJ_Lec_02/Num_1918.java
+++ DataStructure/src/BOJ_Lec_02/Num_1918.java
@@ -4,12 +4,52 @@ import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.io.IOException;
 public class Num_1918 {
-
+	public static int priority(char c) {
+		if(c == ')' || c == '(') {
+			return 0;
+		}
+		else if(c == '+' || c == '-') {
+			return 1;
+		}
+		return 2;
+	}
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		Stack<Character> stack = new Stack<>();
+
 		char[] str = br.readLine().toCharArray();
-				
+		StringBuilder sb = new StringBuilder();
 
+		for(int i = 0; i < str.length; i++) {
+			char c = str[i];
+
+			switch(c) {
+			case '+':
+			case '-':
+			case '*':
+			case '/':
+				while(!stack.isEmpty() && priority(stack.peek()) >= priority(c)) {
+					sb.append(stack.pop());
+				}
+				stack.push(c);
+				break;
+			case '(':
+				stack.push(c);
+				break;
+			case ')':
+				while(!stack.isEmpty() && stack.peek() != '(') {
+					sb.append(stack.pop());
+				}
+				stack.pop();
+				break;
+			default:
+				sb.append(c);
+			}
+			
+		}
+		while(!stack.isEmpty()) {
+			sb.append(stack.pop());
+		}
+		System.out.println(sb.toString());
 	}
-
 }
