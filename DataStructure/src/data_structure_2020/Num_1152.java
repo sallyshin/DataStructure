b75b67961b9452b5e4a0dd7da4d4ca278789b066
sallyshin <46617568+sallyshin@users.noreply.github.com>
2020-04-23 01:17:55 +0900

    단어의 개수

---
+++ DataStructure/src/data_structure_2020/Num_1152.java
@@ -0,0 +1,27 @@
+package data_structure_2020;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashSet;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Set;
+import java.util.StringTokenizer;
+
+public class Num_1152 {
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		int count = 0;
+		while(st.hasMoreTokens()) {
+			String str = st.nextToken();
+			count++;
+		}
+		System.out.println(count);
+	}
+}
