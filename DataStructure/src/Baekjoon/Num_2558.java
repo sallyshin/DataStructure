2ab4da9e0f724fca593545980f4c997758273930
sallyshin <46617568+sallyshin@users.noreply.github.com>
2020-04-24 15:56:13 +0900

    A + B - 2

---
+++ DataStructure/src/Baekjoon/Num_2558.java
@@ -0,0 +1,14 @@
+package Baekjoon;
+import java.io.*;
+import java.util.*;
+public class Num_2558 {
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int a = Integer.parseInt(br.readLine());
+		int b = Integer.parseInt(br.readLine());
+		
+		System.out.println(a + b);
+	}
+
+}
