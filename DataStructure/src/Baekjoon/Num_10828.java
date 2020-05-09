46da2836a9b12619b72c529204864d05919c51e1
sallyshin <46617568+sallyshin@users.noreply.github.com>
2020-05-10 00:22:34 +0900

    스택

--- DataStructure/src/Baekjoon/Num_10828.java
+++ DataStructure/src/Baekjoon/Num_10828.java
@@ -7,7 +7,7 @@ import java.io.IOException;
 import java.util.StringTokenizer;
 
 public class Num_10828 {
-
+// 스택
 	public static void main(String[] args) throws Exception {
 
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
