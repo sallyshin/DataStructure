b631625176ac760565177f2b1ee155883dbf02dc
sallyshin <46617568+sallyshin@users.noreply.github.com>
2020-05-15 20:56:06 +0900

    최대공약수

--- DataStructure/src/Baekjoon/Num_1850.java
+++ DataStructure/src/Baekjoon/Num_1850.java
@@ -3,24 +3,23 @@ import java.util.*;
 import java.io.*;
 public class Num_1850 {
 	// 최대공약수
+	public static long gcd(long a, long b) { // 유클리드 호제법
+		if(b == 0) return a;
+		return gcd(b, a%b);
+	}
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-		long result = 0;
 		StringTokenizer st = new StringTokenizer(br.readLine());
 		long a = Long.parseLong(st.nextToken());
 		long b = Long.parseLong(st.nextToken());
 
 		long min = Math.min(a, b);
+		long max = Math.max(a, b);
 
-		for(long i = 1; i <= min; i++) { // 최대공약수
-			if(a % i == 0 && b % i == 0) {
-				result = i;
-			}
-		}
 		StringBuilder sb = new StringBuilder();
-		for(int i = 0; i < result; i++) {
-			sb.append('1');
+		for(int i = 0; i < gcd(min, max); i++) {
+			sb.append('1'); // 유클리드호제법이 끝날때까지 1이 붙는다
 		}
 		bw.write(sb.toString() + "");
 		br.close();
