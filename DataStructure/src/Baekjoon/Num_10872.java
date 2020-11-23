e14276d9336e505604ccf120c21b160f41c51c75
sallyshin <46617568+sallyshin@users.noreply.github.com>
2020-11-24 04:13:31 +0900

    팩토리얼

---
+++ DataStructure/src/Baekjoon/Num_10872.java
@@ -0,0 +1,23 @@
+//팩토리얼
+//https://www.acmicpc.net/problem/10872
+package Baekjoon;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Num_10872 {
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int n = Integer.parseInt(br.readLine());
+
+		System.out.println(factorial(n));
+	}
+	public static int factorial(int num) {
+		if(num == 0) return 1;
+		int n = num;
+		return n*factorial(n-1);
+	}
+
+}
