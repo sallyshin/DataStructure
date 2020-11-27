30353dffb8e0d3ec3439923a13d2e815f80b464b
sallyshin <46617568+sallyshin@users.noreply.github.com>
2020-11-28 02:52:37 +0900

    소트인사이드

--- DataStructure/src/Baekjoon/Num_1427.java
+++ DataStructure/src/Baekjoon/Num_1427.java
@@ -1,24 +1,33 @@
+// 소트인사이드
+// https://www.acmicpc.net/problem/1427
 package Baekjoon;
 import java.util.*;
 import java.io.*;
 public class Num_1427 {
-	// 소트인사이드
+	
+	static int[] arr;
+	
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-
-		String num = br.readLine();
-		List<Character> list = new ArrayList<>();
-		for(int i = 0; i < num.length(); i++) {
-			list.add(num.charAt(i));
+		String str = br.readLine();
+		
+		arr = new int[10];
+		
+		for(int i = 0; i < str.length(); i++) {
+			arr[str.charAt(i) -'0']++;
 		}
-		Collections.sort(list);
-		for(int i = num.length() - 1; i >= 0; i--) {
-			bw.write(list.get(i) + "");
+		
+		StringBuilder sb = new StringBuilder();
+		// 카운팅 소트
+		
+		for(int i = 0; i < 10; i++) {
+			while(arr[i] > 0) {
+				sb.append(i);
+				arr[i]--;
+			}
 		}
-		bw.flush();
+		System.out.println(sb.reverse());
 		br.close();
-		bw.close();
 	}
 
 }
